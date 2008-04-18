0777223b6efd9c1c80e75cce3267ecb39767038f
Jason Blevins <jrblevin@sdf.lonestar.org>
2008-04-18 09:24:48 -0400

    simple_ddc_vfi: Different transition probabilities for each state; use Rust's VFI approach

--- numeric/simple_ddc_vfi.m
+++ numeric/simple_ddc_vfi.m
@@ -4,13 +4,28 @@
 # Jason Blevins <jrblevin@sdf.lonestar.org>
 # Carrboro, April 14, 2008
 
-# Applies the Bellman operator.
-function v = func(vold, u, P, beta)
-  # Bad health
-  v(1) = u(1,2) + beta * ( P(1,1) * vold(1) + P(1,2) * vold(2) );
+clear;
 
-  # Good health
-  v(2) = u(2,2) + beta * ( P(2,1) * vold(1) + P(2,2) * vold(2) );
+# Applies the Bellman operator for the choice-specific value function V(a, s).
+function V = func(Vold, u, P, beta)
+  maxval = zeros(size(Vold,1));
+  maxval(1) = max(Vold(:,1));
+  maxval(2) = max(Vold(:,2));
+
+  # Fix scaling, state-wise.
+  Vold(:,1) = Vold(:,1) - maxval(1);
+  Vold(:,2) = Vold(:,2) - maxval(2);
+
+  # Apply the Bellman operator.
+  for a = 1:2
+    for s = 1:2
+      V(a, s) = u(a, s) \
+          + beta * log(exp(Vold(1,1)) + exp(Vold(2,1))) * P(1, s, a) \
+          + beta * log(exp(Vold(1,2)) + exp(Vold(2,2))) * P(2, s, a) \
+          - beta * maxval(1) * P(1, s, a) \
+          - beta * maxval(2) * P(2, s, a);
+    endfor
+  endfor
 endfunction
 
 
@@ -18,30 +33,32 @@ endfunction
 function ccp(u, P, beta)
   u
 
-  v = [ 1 2 ];
+  V = [ 1, 2 ;
+        1, 2 ];
   norm = 1;
   while (norm > 1e-6)
-    vold = v;
-    v = func(vold, u, P, beta);
-    norm = max(abs(v - vold) / abs(vold));
+    Vold = V;
+    V = func(Vold, u, P, beta);
+    norm = max( abs(V - Vold) ) / max( abs(Vold) );
   endwhile
-  v
+  norm
+  V
 
   # Choice-specific value function
-  csvf(1,1) = u(1,1) + beta * ( P(1,1) * v(1) + P(1,2) * v(2) );
-  csvf(1,2) = u(1,2) + beta * ( P(1,1) * v(1) + P(1,2) * v(2) );
-  csvf(2,1) = u(2,1) + beta * ( P(2,1) * v(1) + P(2,2) * v(2) );
-  csvf(2,2) = u(2,2) + beta * ( P(2,1) * v(1) + P(2,2) * v(2) );
+#   csvf(1,1) = u(1,1) + beta * ( P(1,1) * v(1) + P(1,2) * v(2) );
+#   csvf(1,2) = u(1,2) + beta * ( P(1,1) * v(1) + P(1,2) * v(2) );
+#   csvf(2,1) = u(2,1) + beta * ( P(2,1) * v(1) + P(2,2) * v(2) );
+#   csvf(2,2) = u(2,2) + beta * ( P(2,1) * v(1) + P(2,2) * v(2) );
 
-  # Scaling
-  maxval(1) = max(csvf(1,:));
-  maxval(2) = max(csvf(2,:));
-  csvf(1,:) = csvf(1,:) - maxval(1);
-  csvf(2,:) = csvf(2,:) - maxval(2);
+#   # Scaling
+#   maxval(1) = max(csvf(1,:));
+#   maxval(2) = max(csvf(2,:));
+#   csvf(1,:) = csvf(1,:) - maxval(1);
+#   csvf(2,:) = csvf(2,:) - maxval(2);
 
   # Pr(smoke | health)
-  ccp(1) = exp(csvf(1,1)) / (exp(csvf(1,1)) + exp(csvf(1,2)));
-  ccp(2) = exp(csvf(2,1)) / (exp(csvf(2,1)) + exp(csvf(2,2)));
+  ccp(1) = exp(V(1,1)) / ( exp(V(1,1)) + exp(V(2,1)) );
+  ccp(2) = exp(V(1,2)) / ( exp(V(1,2)) + exp(V(2,2)) );
   ccp
 endfunction
 
@@ -49,14 +66,20 @@ endfunction
 # Discount rate
 beta = 0.95;
 
-# u is the payoff matrix: [ bad, good ] x [ smoke, not ]
-u = [ 0  3; 1 4 ];
+# u(a, x) is the payoff from choosing action a in ( smoke, not )
+# while being in state x \in ( bad, good ).
+u = [ 0   0 ;
+      3   5 ];
 
-# State transition matrix, conditional on smoking behavior:
-# Pr(health | smoking) for [ bad, good ] x [ smoke, not ]
-P = [ 0.8  0.2 ;
-      0.1  0.9 ];
+# State transition matrix, conditional on smoking and not smoking respectively:
+# Pr(x' | x, a) for [ bad, good ] x [ bad, good ] x [ smoke, not ]
+P = zeros(2,2,2);
+
+P(:,:,1) = [ 0.8  0.2 ;
+             0.5  0.5 ];
+
+P(:,:,2) = [ 0.5  0.5 ;
+             0.2  0.8 ];
 
 # Find the conditional choice probabilities for the given beta, u, and P.
 ccp(u, P, beta)
-
