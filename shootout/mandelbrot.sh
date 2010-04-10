369f0f840839ea1d1043cb509933c885c1682b28
Jason Blevins <jrblevin@sdf.lonestar.org>
2010-04-10 17:23:51 -0400

    shootout/mandelbrot.sh: save output to id-specific files

--- shootout/mandelbrot.sh
+++ shootout/mandelbrot.sh
@@ -3,7 +3,7 @@
 id=$1
 
 rm mandelbrot
-ifort -fast -openmp -o mandelbrot mandelbrot.ifc-$1.f90
-time ./mandelbrot 200 > mandelbrot.pbm
-diff mandelbrot-output.txt mandelbrot.pbm
-time ./mandelbrot 16000 > /tmp/foo.pbm
+ifort -O3 -static -fast -openmp -o mandelbrot mandelbrot.ifc-$1.f90
+time ./mandelbrot 200 > mandelbrot-$1.pbm
+diff mandelbrot-output.txt mandelbrot-$1.pbm
+time ./mandelbrot 16000 > /tmp/mandelbrot-$1.pbm
