8c2723da58088f3b003d3a2f174546c6d2c886e6
Jason Blevins <jrblevin@sdf.lonestar.org>
2010-04-10 16:57:12 -0400

    shootout: import all Fortran versions of mandelbrot

--- shootout/mandelbrot.sh
+++ shootout/mandelbrot.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-ifort -fast -openmp -o mandelbrot mandelbrot.f90
+id=$1
+
+rm mandelbrot
+ifort -fast -openmp -o mandelbrot mandelbrot.ifc-$1.f90
 time ./mandelbrot 200 > mandelbrot.pbm
 diff mandelbrot-output.txt mandelbrot.pbm
 time ./mandelbrot 16000 > /tmp/foo.pbm
