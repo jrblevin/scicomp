cd33322b6fed991f16bec1e1f426e6da7acb7991
Jason Blevins <jrblevin@sdf.lonestar.org>
2010-04-10 17:31:08 -0400

    shootout/mandelbrot.sh: use id-specific binaries also

--- shootout/mandelbrot.sh
+++ shootout/mandelbrot.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-id=$1
-
-rm mandelbrot
-ifort -O3 -static -fast -openmp -o mandelbrot mandelbrot.ifc-$1.f90
-time ./mandelbrot 200 > mandelbrot-$1.pbm
+rm mandelbrot-$1
+ifort -O3 -static -fast -openmp -o mandelbrot-$1 mandelbrot.ifc-$1.f90
+time ./mandelbrot-$1 200 > mandelbrot-$1.pbm
 diff mandelbrot-output.txt mandelbrot-$1.pbm
-time ./mandelbrot 16000 > /tmp/mandelbrot-$1.pbm
+time ./mandelbrot-$1 16000 > /tmp/mandelbrot-$1.pbm
