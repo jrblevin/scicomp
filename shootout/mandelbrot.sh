ec0c28a8e694ef98799f24e6e2fd723192f36b55
Jason Blevins <jrblevin@sdf.lonestar.org>
2010-04-11 15:49:45 -0400

    shootout: Makefile (mandelbrot only)

--- shootout/mandelbrot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm mandelbrot-$1
-ifort -O3 -static -fast -openmp -o mandelbrot-$1 mandelbrot.ifc-$1.f90
-time ./mandelbrot-$1 200 > mandelbrot-$1.pbm
-diff mandelbrot-output.txt mandelbrot-$1.pbm
-time ./mandelbrot-$1 16000 > /tmp/mandelbrot-$1.pbm
