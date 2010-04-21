ca1adec1b9bfe3f026dfe19ea2d06847a1376fbc
Jason Blevins <jrblevin@sdf.lonestar.org>
2010-04-21 01:25:13 -0400

    shootout/mandelbrot: vectorize inner loop

--- shootout/mandelbrot.ifc-4.f90
+++ shootout/mandelbrot.ifc-4.f90
@@ -12,11 +12,13 @@ program mandelbrot
   integer, parameter :: int8 = selected_int_kind(2)
   integer, parameter :: iter = 50
   real(dp), parameter :: limit2 = 4.0_dp
-  character(len=8) :: argv
-  integer :: n, x, y, i, j, pos
-  integer(int8) :: byte
-  real(dp) :: inv_2n, Zi, Zr, Ti, Tr, Cr, Ci
+  integer, dimension(8), parameter :: incr = (/ 0, 1, 2, 3, 4, 5, 6, 7 /)
+  real(dp), dimension(8) :: Zi, Zr, Ti, Tr, Cr, Ci
   integer(int8), dimension(:,:), allocatable :: buf
+  character(len=8) :: argv
+  integer :: n, x, y, i, pos
+  integer(int8) :: byte
+  real(dp) :: inv_2n
 
   ! read dimension from command line
   call get_command_argument(1, argv)
@@ -36,35 +38,27 @@ program mandelbrot
      pos = 0
      Ci = inv_2n * y - 1.0_dp
 
-     do x = 0, n - 1, 8 ! ASSUMING DIVISIBLE BY 8
+     do x = 0, n - 1, 8
 
-        byte = 0_int8
+        Zr = 0.0_dp
+        Zi = 0.0_dp
+        Tr = 0.0_dp
+        Ti = 0.0_dp
 
-        do j = 0, 7
+        Cr = inv_2n * (x + incr) - 1.5_dp
 
-           Zr = 0.0_dp
-           Zi = 0.0_dp
-           Tr = 0.0_dp
-           Ti = 0.0_dp
+        do i = 1, iter
+           Zi = 2.0_dp * Zr * Zi + Ci
+           Zr = Tr - Ti + Cr
+           Ti = Zi * Zi
+           Tr = Zr * Zr
+        end do
 
-           Cr = inv_2n * (x + j) - 1.5_dp
-           do i = 1, iter
-              Zi = 2.0_dp * Zr * Zi + Ci
-              Zr = Tr - Ti + Cr
-              Ti = Zi * Zi
-              Tr = Zr * Zr
-              if (Tr + Ti > limit2) then
-                 exit
-              end if
-           end do
-
-           ! We're in the set, set this bit to 0
-!           if (i > iter) byte = ibset(byte, 7 - j)
+        do i = 1, 8
            byte = ishft(byte, 1)
-           if (i > iter) then
+           if (Tr(i) + Ti(i) < limit2) then
               byte = ior(byte, 1)
            end if
-
         end do
 
         pos = pos + 1
